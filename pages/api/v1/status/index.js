c938a158c936ae7b403c322b2aeba28749ddcfe5
Brendo Filemon de Carvalho Monteiro <brendofcmonteiro@gmail.com>
2024-09-24 19:50:35 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,17 +1,41 @@
 //Criando uma rota pública: Basta criar um arquivo dentro da pasta /pages/api
 // Quando a api /status for consultada é bom fazer um teste de conexão com o banco de dados, para isso importamos o database.js para a rota da api
 
-import database from "infra/database.js";
+import database from "infra/database";
 
 export default async function status(request, response) {
-  //Testando uma query
+  //Criando requisições que serão obtidas na requisição da api /status
+  const updatedAt = new Date().toISOString();
 
-  const result = await database.query("SELECT 1 + 1 AS sum;");
-  console.log(result.rows[0]);
-  //Definindo uma resposta para uma requisição a api /status
-  response.status(200).json({ chave: "Requisição bem sucedida!" }); //Código 200 - > Requisição deu certo.
-  //O método .send envia a informação passada
-  //O método .json envia a informação passada em forma de arquivo JSON no formato "chave: valor" para o navegador assumindo já o formato utf-8
+  //Comando SHOW na query para buscar os dados de configurações do banco, nesse caso a versão do postgres.
+  const pgVersionQuery = await database.query("SHOW server_version;");
+  const pgVersionValue = pgVersionQuery.rows[0].server_version;
+
+  //Query para buscar a quantidade máxima de conexões
+  const maxConnectionsQuery = await database.query("SHOW max_connections;");
+  const maxConnectionsValue = maxConnectionsQuery.rows[0].max_connections;
+
+  // Passando o nome do banco de dados para a consulta da query a partir da variável de ambiente. Assim quando o banco estiver em ambiente de produção, será feito automaticamente
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConnectionsQuery = await database.query({
+    // "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';", -> query hardcoded
+    //::int converte o valor para inteiro já no banco
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+    // O valor do array irá substituir o placeholder "$1"
+  });
+
+  const openedConnectionsValue = openedConnectionsQuery.rows[0].count;
+
+  //Definindo uma resposta para uma requisição a api /status (Retorno do endpoint)
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        postgres_version: pgVersionValue,
+        max_connections: +maxConnectionsValue,
+        opened_connections: openedConnectionsValue,
+      },
+    },
+  }); //Código 200 - > Requisição deu certo.
 }
-
-//CURL -> É um Client que faz requisições HTTP
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,4 +3,17 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  //Para ver o corpo da requisição, o que de fato está retornando o fetch
+  const responseBody = await response.json();
+
+  //Confere se o valor de updated_at é vazio, null ou zero e compara com o valor padrão de ISOString gerando erro caso não tenha uma data válida
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database.postgres_version).toEqual("16.0");
+
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
